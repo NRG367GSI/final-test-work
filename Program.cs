9324cfc622b37b9f79cbd94d6f6631093134a984
Сергей Игоревич Гаврилов <109974689+NRG367GSI@users.noreply.github.com>
2022-09-17 00:19:29 +0300

    addad programm

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿string[] Sequence(string[] seq)
+{
+    int quantity = 0;
+    for (int i = 0; i < seq.Length; i++)
+    {
+        if (seq[i].Length <= 3) quantity++;
+    }
+    string[] newSeq = new string[quantity];
+    quantity = 0;
+    for (int j = 0; j < seq.Length; j++)
+    {
+        if (seq[j].Length <= 3)
+        {
+            newSeq[quantity] = seq[j];
+            quantity++;
+        }
+    }
+    return newSeq;
+}
+
+void OutputSequence(string[] seq)
+{
+    for (int i =0; i < seq.Length; i++)
+    {
+        Console.Write($" {seq[i]}, ");
+    }
+}
+
+string[] rows = {"as", "asdfgh", "asd"};
+
+OutputSequence(Sequence(rows));
